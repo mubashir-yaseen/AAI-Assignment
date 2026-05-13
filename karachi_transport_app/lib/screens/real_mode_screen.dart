46173e4a3f5dbf4df8f84fecfb07b7e2fd10e1f1
Muhammad Mubashir <149854409+mubashir-yaseen@users.noreply.github.com>
2026-05-14 02:54:27 +0500

    Update real_mode_screen.dart

--- karachi_transport_app/lib/screens/real_mode_screen.dart
+++ karachi_transport_app/lib/screens/real_mode_screen.dart
@@ -5,7 +5,8 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 import '../widgets/route_summary_card.dart';
 
-const String baseUrl = "http://localhost:8000";
+const String baseUrl = "https://aai-assignment-backend.onrender.com"; // Replace with your actual Render URL
+//const String baseUrl = "http://localhost:8000"; // Previous code when i was running on localhost
 
 class RealModeScreen extends StatefulWidget {
   const RealModeScreen({super.key});
